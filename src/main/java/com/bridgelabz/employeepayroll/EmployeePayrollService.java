9dadea76b847fcda464de885d817ced4e275cb16
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-06 15:36:34 +0530

    [Daxil] Added Function to read data from console and write to console

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -1,9 +1,47 @@
 package com.bridgelabz.employeepayroll;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+
+	// enum containing fixed set of constants
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
+	public static List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+	}
 
 	public static void main(String[] args) {
 
+		employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+
 	}
 
+	// Function to read Employee Payroll Data from Console
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	// Function to write Employee Payroll Data to Console
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	}
 }
